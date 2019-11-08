6f55e2f16262de42274790a0cd91a5b3733e1c96
Robert Baker <zanotam@gmail.com>
2019-11-07 17:41:14 -0700

    Now debounces search requests so as to not exceed API limit too soon.

--- src/actions/search.js
+++ src/actions/search.js
@@ -2,14 +2,15 @@ import searchYouTube from '../lib/searchYouTube.js';
 import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
+import { debounce } from 'lodash';
 
 var handleVideoSearch = (q) => {
-  return (dispatch) => {
+  return debounce((dispatch) => (
     searchYouTube({key: YOUTUBE_API_KEY, query: q}, (videos) => {
-      changeVideo(videos[0]);
-      changeVideoList(videos);
-    });
-  };
+      dispatch(changeVideo(videos[0]));
+      dispatch(changeVideoList(videos));
+    })
+  ), 500);
 };
 
 export default handleVideoSearch;
